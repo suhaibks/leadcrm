c040f97f8731189cf245668faa6ebe08e2506376
suhaibks <suhaibcoorg5@gmail.com>
2025-09-16 14:54:42 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// replace <repo-name> with your GitHub repo name, here it's 'leadcrm'
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: { '@': '/src' }, // optional: import from '@/...' instead of relative paths
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
-  preview: {
-    port: 5173,
-  },
+  base: '/leadcrm/',   // 👈 important
 })
